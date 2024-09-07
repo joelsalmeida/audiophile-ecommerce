c698972682ab33b91b149070ee70e69cda110da0
Joel Almeida <joel.almeida.sa@gmail.com>
2024-09-07 15:34:15 -0300

    refactor: Update Button component to support both button and link functionality

--- src/components/inputs/button/index.tsx
+++ src/components/inputs/button/index.tsx
@@ -1,10 +1,49 @@
-import type { ButtonProps } from './index.types';
+import type {
+  ButtonAsLinkProps,
+  ButtonAsButtonProps,
+  ButtonProps,
+} from './index.types';
 import styles from './index.module.scss';
 import Image from 'next/image';
+import Link from 'next/link';
 
-export function Button({ children, variant, ...restProps }: ButtonProps) {
+export function Button({
+  children,
+  variant,
+  asLink,
+  ...restProps
+}: ButtonProps) {
+  if (asLink) {
+    const { href, ...linkProps } = restProps as ButtonAsLinkProps;
+
+    return (
+      <Link
+        href={href}
+        className={styles.button}
+        data-variant={variant}
+        {...linkProps}
+      >
+        {children}
+
+        {variant === 'text' && (
+          <Image
+            className={styles.button__icon}
+            src="/arrow-right-icon.svg"
+            alt="Arrow right"
+            width={5}
+            height={10}
+          />
+        )}
+      </Link>
+    );
+  }
+
   return (
-    <button className={styles.button} data-variant={variant} {...restProps}>
+    <button
+      className={styles.button}
+      data-variant={variant}
+      {...(restProps as ButtonAsButtonProps)}
+    >
       {children}
 
       {variant === 'text' && (
--- src/components/inputs/button/index.types.ts
+++ src/components/inputs/button/index.types.ts
@@ -1,6 +1,18 @@
 import { ButtonHTMLAttributes, ReactNode } from 'react';
+import { LinkProps } from 'next/link';
 
-export type ButtonProps = {
+export type ButtonBaseProps = {
   variant: 'text' | 'contained' | 'outlined';
   children: ReactNode;
+};
+
+export type ButtonAsButtonProps = ButtonBaseProps & {
+  asLink?: false;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export type ButtonAsLinkProps = ButtonBaseProps & {
+  asLink: true;
+  href: LinkProps['href'];
+} & Omit<LinkProps, 'href'>;
+
+export type ButtonProps = ButtonAsButtonProps | ButtonAsLinkProps;
