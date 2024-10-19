f6d52e4003a847a09a62de5bbce4349cfb818581
Joel Almeida <joel.almeida.sa@gmail.com>
2024-10-19 12:12:15 -0300

    refactor: Update app layout and page components
    Refactor the layout component to include a header and footer. Also, update the page component to include a hero section, category cards, product highlights, and an about section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,38 @@
-import styles from "./page.module.scss";
+import { Hero } from '@/components/hero';
+import styles from './page.module.scss';
+import {
+  CategoryCardContainer,
+  CategoryCardItem,
+} from '@/components/category-card';
+import { CATEGORIES_DATA } from './category/[slug]/data';
+import { AboutSection } from '@/components/about-section';
+import { ProductHighlightFull } from '@/components/product-highlight/product-highlight-full';
+import { ProductHighlightSingle } from '@/components/product-highlight/product-highlight-single';
+import { ProductHighlightDouble } from '@/components/product-highlight/product-highlight-double';
+import { ZX9, ZX7, YX1 } from '@/components/product-highlight/data';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>AUDIOPHILE E-COMMERCE</h1>
+    <>
+      <Hero />
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla est
-        impedit odit minima exercitationem, dolore commodi quae, voluptas
-        accusantium, esse cum id consectetur delectus. Cum delectus illum
-        blanditiis dolor beatae?
-      </p>
-    </main>
+      <main className={styles.main}>
+        <CategoryCardContainer>
+          {CATEGORIES_DATA.map((category) => (
+            <CategoryCardItem {...category} key={category.title} />
+          ))}
+        </CategoryCardContainer>
+
+        <section>
+          <ProductHighlightFull {...ZX9} />
+
+          <ProductHighlightSingle {...ZX7} />
+
+          <ProductHighlightDouble {...YX1} />
+        </section>
+
+        <AboutSection />
+      </main>
+    </>
   );
 }
