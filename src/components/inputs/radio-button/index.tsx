91e1a20a05fb6e6db07346adebea08419e600108
Joel Almeida <joel.almeida.sa@gmail.com>
2024-12-13 15:54:47 -0300

    refactor: Update RadioButton and TexField components with forwardRef

--- src/components/inputs/radio-button/index.tsx
+++ src/components/inputs/radio-button/index.tsx
@@ -1,18 +1,21 @@
+import { forwardRef } from 'react';
 import styles from './index.module.scss';
 import { RadioButtonProps } from './index.types';
 
-export function RadioButton({
-  label: labelValue,
-  ...restProps
-}: RadioButtonProps) {
-  return (
-    <label className={styles.radioButton}>
-      <input
-        type="radio"
-        className={styles.radioButton__input}
-        {...restProps}
-      />
-      {labelValue}
-    </label>
-  );
-}
+export const RadioButton = forwardRef<HTMLInputElement, RadioButtonProps>(
+  ({ label: labelValue, ...restProps }, ref) => {
+    return (
+      <label className={styles.radioButton}>
+        <input
+          type="radio"
+          className={styles.radioButton__input}
+          {...restProps}
+          ref={ref}
+        />
+        {labelValue}
+      </label>
+    );
+  },
+);
+
+RadioButton.displayName = 'RadioButton';
--- src/components/inputs/text-field/index.tsx
+++ src/components/inputs/text-field/index.tsx
@@ -1,13 +1,19 @@
+import { forwardRef } from 'react';
 import { TextFieldProps } from './index.types';
 import styles from './index.module.scss';
 
-export function TextField({ error = false, ...restProps }: TextFieldProps) {
-  return (
-    <input
-      type="text"
-      className={styles.textField}
-      data-error={error}
-      {...restProps}
-    />
-  );
-}
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  ({ error = false, ...restProps }, ref) => {
+    return (
+      <input
+        type="text"
+        className={styles.textField}
+        data-error={error}
+        {...restProps}
+        ref={ref}
+      />
+    );
+  },
+);
+
+TextField.displayName = 'TextField';
