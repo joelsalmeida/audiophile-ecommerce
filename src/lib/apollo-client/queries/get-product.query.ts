4d16d081c5ecbd134f162d79579d708ae2be5287
Joel Almeida <joel.almeida.sa@gmail.com>
2025-01-03 09:18:23 -0300

    feat: Add GET_PRODUCT_QUERY for fetching product details

---
+++ src/lib/apollo-client/queries/get-product.query.ts
@@ -0,0 +1,50 @@
+import { gql } from '@apollo/client';
+
+export const GET_PRODUCT_QUERY = gql`
+  query GetProduct($productId: String!) {
+    product(id: $productId) {
+      _id
+      boxContent {
+        name
+        quantity
+      }
+      category
+      description
+      features
+      galleryImages {
+        imageOne {
+          alt
+          paths {
+            large
+            medium
+            small
+          }
+        }
+        imageThree {
+          paths {
+            large
+            medium
+            small
+          }
+        }
+        imageTwo {
+          paths {
+            large
+            medium
+            small
+          }
+        }
+      }
+      name
+      previewImage {
+        paths {
+          large
+          medium
+          small
+        }
+      }
+      price
+      releaseDate
+    }
+  }
+`;
---
+++ src/lib/apollo-client/queries/index.ts
@@ -0,0 +1,1 @@
+export { GET_PRODUCT_QUERY } from './get-product.query';
