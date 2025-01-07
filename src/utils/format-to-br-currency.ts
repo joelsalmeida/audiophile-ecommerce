3656bb8248bd2af4e0cf2c461d2a8d09b063de1a
Joel Almeida <joel.almeida.sa@gmail.com>
2025-01-07 15:20:10 -0300

    feat: Add utility function to format values as Brazilian currency

---
+++ src/utils/format-to-br-currency.ts
@@ -0,0 +1,6 @@
+export function formatToBrCurrency(valueInCents: number) {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(valueInCents / 100);
+}
