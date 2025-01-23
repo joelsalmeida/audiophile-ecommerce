6539b727f8f0a0b8f8258684044fcdd0b7ff5beb
Joel Almeida <joel.almeida.sa@gmail.com>
2025-01-23 12:44:35 -0300

    feat: Enhance AddToCart component with quantity management and mutation integration

--- src/components/add-to-cart/index.tsx
+++ src/components/add-to-cart/index.tsx
@@ -3,17 +3,48 @@ import styles from './index.module.scss';
 import { Button } from '../inputs/button';
 import { QuantitySelector } from '../inputs/quantity-selector';
 import { AddToCartProps } from './index.types';
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_TO_CART_MUTATION } from '@/lib/apollo-client/mutations';
 
 // TODO: Improve on click handler logic
-export function AddToCart({ price, onClickHandler }: AddToCartProps) {
+export function AddToCart({ productId, price }: AddToCartProps) {
+  const [quantity, setQuantity] = useState(1);
+  const [addToCartMutation] = useMutation(ADD_TO_CART_MUTATION);
+
+  function increment() {
+    setQuantity((prevValue) => prevValue + 1);
+  }
+
+  function decrement() {
+    setQuantity((prevValue) => prevValue - 1);
+  }
+
+  const addToCartHandler = async (id: string) => {
+    const addToCartInput = {
+      _id: id,
+      quantity,
+    };
+
+    const { data, errors } = await addToCartMutation({
+      variables: { addToCartInput },
+    });
+
+    console.log('##### addToCartHandler : ', data, errors);
+  };
+
   return (
     <div className={styles.addToCart}>
       <span className={styles.addToCart__price}>{price}</span>
 
       <div className={styles.addToCart__addSection}>
-        <QuantitySelector />
+        <QuantitySelector
+          increment={increment}
+          decrement={decrement}
+          currentQuantity={quantity}
+        />
 
-        <Button variant="contained" onClick={onClickHandler}>
+        <Button variant="contained" onClick={() => addToCartHandler(productId)}>
           Add to cart
         </Button>
       </div>
--- src/components/add-to-cart/index.types.ts
+++ src/components/add-to-cart/index.types.ts
@@ -1,4 +1,4 @@
 export type AddToCartProps = {
   price: string;
-  onClickHandler: () => void;
+  productId: string;
 };
