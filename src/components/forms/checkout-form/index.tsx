82b04daf331f4fd4d5a085597e6f2de537f8fa8e
Joel Almeida <joel.almeida.sa@gmail.com>
2026-01-10 11:39:40 -0300

    refactor(checkout): compose checkout form using section components

--- src/components/forms/checkout-form/index.tsx
+++ src/components/forms/checkout-form/index.tsx
@@ -1,45 +1,44 @@
 'use client';
 
-import { RadioButton } from '@/components/inputs/radio-button';
-import { TextField } from '@/components/inputs/text-field';
-import styles from './index.module.scss';
-import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import { CheckoutModal } from '@/components/modals';
+import { useModalBaseActions } from '@/contexts/modal-base-context';
+import { useCartResumeData, useGetZipCodeData } from '@/custom-hooks';
+import { PLACE_ORDER_MUTATION } from '@/lib/apollo-client/mutations';
+import { GET_CART_QUERY } from '@/lib/apollo-client/queries';
+import { useMutation } from '@apollo/client';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useEffect } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import styles from './index.module.scss';
 import {
   CheckoutFormDataType,
   checkoutFormSchema,
 } from './schemas/checkoutFormSchema';
-import { useCartResumeData, useGetZipCodeData } from '@/custom-hooks';
-import { maskPhoneNumber, maskZipCode } from '@/utils';
-import { useModalBaseActions } from '@/contexts/modal-base-context';
-import { CheckoutModal } from '@/components/modals';
-import { useEffect } from 'react';
-import Image from 'next/image';
-import { useMutation } from '@apollo/client';
-import { PLACE_ORDER_MUTATION } from '@/lib/apollo-client/mutations';
-import { GET_CART_QUERY } from '@/lib/apollo-client/queries';
+import { BillingSection } from './sections/billing-section';
+import { PaymentSection } from './sections/payment-section';
+import { ShippingSection } from './sections/shipping-section';
 
 export function CheckoutForm() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    setValue,
-    control,
-    formState: { errors, isSubmitSuccessful },
-    reset,
-  } = useForm<CheckoutFormDataType>({
+  const checkoutForm = useForm<CheckoutFormDataType>({
     resolver: zodResolver(checkoutFormSchema),
     defaultValues: {
       paymentMethod: 'e-money',
     },
   });
 
+  const {
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+  } = checkoutForm;
+
   const { openModalBase } = useModalBaseActions();
 
   console.log('##### ERRORS: ', errors);
 
-  const [ZIP_CODE, PAYMENT_METHOD] = watch(['zipCode', 'paymentMethod']);
+  const [ZIP_CODE] = watch(['zipCode', 'paymentMethod']);
 
   const {
     data: zipCodeData,
@@ -83,175 +82,17 @@ export function CheckoutForm() {
     if (isSubmitSuccessful) reset();
   }, [isSubmitSuccessful, reset]);
 
-  const IS_CASH_ON_DELIVERY = PAYMENT_METHOD === 'cash-on-delivery';
-
   return (
     <form
       id="checkout-form"
       className={styles.checkoutForm}
       onSubmit={handleSubmit(handleOnSubmit)}
     >
-      <div className={styles.checkoutForm__section}>
-        <strong className={styles.checkoutForm__section__title}>
-          Checkout
-        </strong>
+      <BillingSection checkoutForm={checkoutForm} />
 
-        <h2>Billing Details</h2>
+      <ShippingSection checkoutForm={checkoutForm} />
 
-        <div className={styles.checkoutForm__inputContainer}>
-          <label className={styles.checkoutForm__label}>
-            Name
-            <TextField
-              {...register('name')}
-              placeholder="John Doe"
-              error={Boolean(errors.name?.message)}
-            />
-          </label>
-
-          <label className={styles.checkoutForm__label}>
-            E-mail
-            <TextField
-              {...register('email')}
-              placeholder="john.doe@mail.com"
-              error={Boolean(errors.email?.message)}
-            />
-          </label>
-        </div>
-
-        <label className={styles.checkoutForm__label}>
-          Phone Number
-          <Controller
-            name="phone"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                inputMask={maskPhoneNumber}
-                placeholder="(11) 98765-4321"
-                error={Boolean(errors.phone?.message)}
-              />
-            )}
-          />
-        </label>
-      </div>
-
-      <div className={styles.checkoutForm__section}>
-        <h2>Shipping info</h2>
-
-        <label className={styles.checkoutForm__label}>
-          Address
-          <TextField
-            {...register('address')}
-            placeholder="1137 Williams Avenue"
-            error={Boolean(errors.address?.message)}
-          />
-        </label>
-
-        <div className={styles.checkoutForm__inputContainer}>
-          <label className={styles.checkoutForm__label}>
-            Zip Code
-            <Controller
-              name="zipCode"
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  inputMask={maskZipCode}
-                  placeholder="98987-654"
-                  error={Boolean(errors.zipCode?.message)}
-                />
-              )}
-            />
-          </label>
-
-          <label className={styles.checkoutForm__label}>
-            City
-            <TextField
-              {...register('city')}
-              placeholder="New York"
-              error={Boolean(errors.city?.message)}
-            />
-          </label>
-        </div>
-
-        <label className={styles.checkoutForm__label}>
-          Country
-          <TextField
-            {...register('country')}
-            placeholder="United States"
-            error={Boolean(errors.country?.message)}
-          />
-        </label>
-      </div>
-
-      <div className={styles.checkoutForm__section}>
-        <h2>Payment details</h2>
-
-        <div className={styles.checkoutForm__inputContainer}>
-          <span className={styles.checkoutForm__label}>Payment Method</span>
-
-          <Controller
-            name="paymentMethod"
-            control={control}
-            render={({ field }) => (
-              <div className={styles.checkoutForm__inputContainer__item}>
-                <RadioButton
-                  {...field}
-                  label="e-Money"
-                  id="e-money"
-                  value="e-money"
-                  checked={field.value === 'e-money'}
-                />
-
-                <RadioButton
-                  {...field}
-                  label="Cash on Delivery"
-                  id="cash-on-delivery"
-                  value="cash-on-delivery"
-                  checked={field.value === 'cash-on-delivery'}
-                />
-              </div>
-            )}
-          />
-        </div>
-
-        <div className={styles.checkoutForm__inputContainer}>
-          <label className={styles.checkoutForm__label}>
-            e-Money Number
-            <TextField
-              {...register('eMoneyNumber')}
-              placeholder="238521993"
-              error={Boolean(errors.eMoneyNumber?.message)}
-            />
-          </label>
-
-          <label className={styles.checkoutForm__label}>
-            e-Money PIN
-            <TextField
-              {...register('eMoneyPin')}
-              placeholder="6891"
-              error={Boolean(errors.eMoneyPin?.message)}
-            />
-          </label>
-        </div>
-      </div>
-
-      {IS_CASH_ON_DELIVERY && (
-        <div className={styles.checkoutForm__paymentInfo}>
-          <Image
-            src="/checkout/icon-cash-on-delivery.svg"
-            alt=""
-            width={48}
-            height={48}
-          />
-
-          <p>
-            The ‘Cash on Delivery’ option enables you to pay in cash when our
-            delivery courier arrives at your residence. Just make sure your
-            address is correct so that your order will not be cancelled.
-          </p>
-        </div>
-      )}
+      <PaymentSection checkoutForm={checkoutForm} />
     </form>
   );
 }
